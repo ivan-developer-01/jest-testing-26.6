90489102ad8051bb9056eaa2d930a151e2e5abb8
Ivan <127977316+ivan-developer-01@users.noreply.github.com>
2023-10-26 12:52:51 +0300

    add random tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,19 @@
 const sum = require("./index");
-test("adds 1 + 2 to equal 3", () => {
-	expect(sum(1, 2)).toBe(3);
-});
+const inputs = [];
+
+for (let i = 0; i < 20; i++) {
+	inputs.push([getRandomInt(100), getRandomInt(100)]);
+}
+
+const outputs = inputs.map(([a, b]) => a + b);
+
+for (let i = 0; i < inputs.length; i++) {
+	const item = inputs[i];
+	it(`adds ${item[0]} + ${item[1]} to equal ${outputs[i]}`, () => {
+		expect(sum(item[0], item[1])).toBe(outputs[i]);
+	});
+}
+
+function getRandomInt(max) {
+	return Math.floor(Math.random() * Math.floor(max));
+}
